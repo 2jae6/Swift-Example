37e50793ca22dc94dea142f92d0337ae19b72c17
JaeWook Lee <2jae66@gmail.com>
2020-01-10 16:16:42 +0900

    뷰컨트롤러 간 데이터 전달 동기방식

---
+++ SendValue/SendValue/SecondVC.swift
@@ -0,0 +1,9 @@
+//
+//  SecondVC.swift
+//  SendValue
+//
+//  Created by 1 on 2020/01/09.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import Foundation
---
+++ SendValue/SendValue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendValue
+//
+//  Created by 1 on 2020/01/09.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
