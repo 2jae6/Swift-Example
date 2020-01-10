c15c001c41d90da7ec9a0d7ae5d22a2bba0101c4
JaeWook Lee <2jae66@gmail.com>
2020-01-10 16:17:09 +0900

    뷰 컨트롤러 간 데이터 주고 받기 동기방식

--- SendValue/SendValue/SecondVC.swift
+++ SendValue/SendValue/SecondVC.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2020 wook. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SecondVC: UIViewController{
+    
+    
+    @IBOutlet weak var resultTF: UILabel!
+    @IBOutlet weak var resultSwit: UILabel!
+    @IBOutlet weak var resultStepper: UILabel!
+    
+    
+    var paramTF: String = ""
+    var paramSwit: Bool = false
+    var paramStepper: Double = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.resultTF.text = paramTF
+        self.resultSwit.text = (self.paramSwit == true ? "자동갱신" : "자동갱신안함")
+        self.resultStepper.text = "\(Int(paramStepper)) 마다 갱신"
+    }
+    
+    
+    @IBAction func backButton(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
+    }
+    
+    
+}
--- SendValue/SendValue/ViewController.swift
+++ SendValue/SendValue/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+ 
+    @IBOutlet weak var tf: UITextField!
+    @IBOutlet weak var swc: UISwitch!
+    @IBOutlet weak var sp: UIStepper!
+    
+    @IBOutlet weak var updateText: UILabel!
+    @IBOutlet weak var intervalText: UILabel!
+    
+    
+    @IBAction func onSwitch(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            self.updateText.text = "갱신"
+        }else{
+            self.updateText.text = "갱신하지 않음"
+        }
     }
-
-
+    @IBAction func onStepper(_ sender: UIStepper) {
+        let value = Int(sender.value)
+        self.intervalText.text = "\(value)마다."
+    }
+    
+    @IBAction func submitButton(_ sender: Any) {
+        guard let vc2 = self.storyboard?.instantiateViewController(withIdentifier: "VC2") as? SecondVC else{
+            return
+        }
+        
+        vc2.paramTF = self.tf.text!
+        vc2.paramSwit = self.swc.isOn
+        vc2.paramStepper = self.sp.value
+        
+        self.present(vc2, animated: true)
+        
+    }
 }
 
