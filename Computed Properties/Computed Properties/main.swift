23d2666ddb4c7a130a3389e68bac1fd9f2a94616
JaeWook Lee <2jae66@gmail.com>
2020-08-22 19:52:32 +0900

    computed properties

---
+++ Computed Properties/Computed Properties/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Computed Properties
+//
+//  Created by 1 on 2020/08/22.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
