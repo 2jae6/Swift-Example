ebd78fd59991a414ca97eb863dbcff8e062ac4b3
JaeWook Lee <2jae66@gmail.com>
2020-09-19 19:51:38 +0900

    example

---
+++ StartDevelopingiOSApps/StartDevelopingiOSApps/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  StartDevelopingiOSApps
+//
+//  Created by 1 on 2020/09/15.
+//  Copyright © 2020 wook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
